e3554f5e92dc48814b28dd969ad329afa4ec3552
ogtopramen <noreply>
2015-01-07 01:20:09 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -15,14 +15,14 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         }
     },
 
-    handleIncrementAction: {
+    handleIncAction: {
         value: function (event) {
             var value = Number(this.templateObjects.value.element.textContent);
             this.templateObjects.value.element.textContent = value + 1;
         }
     },
 
-    handleDecrementAction: {
+    handleDecAction: {
         value: function (event) {
             var value = Number(this.templateObjects.value.element.textContent);
             this.templateObjects.value.element.textContent = value - 1;
