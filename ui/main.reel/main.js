073a49cd856c196260d7041de7b2111df5eadf81
ogtopramen <noreply>
2015-01-07 01:58:51 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -25,9 +25,9 @@ exports.Main = Component.specialize(/** @lends Main# */ {
 
     handleDecAction: {
         value: function (event) {
-            var value = Number(this.templateObjects.value.element.textContent);
+            var value = Number(this.templateObjects.val.element.textContent);
             var value2 = Number(this.templateObjects.value2.element.textContent);
-            this.templateObjects.value.element.textContent = value - value2;
+            this.templateObjects.val.element.textContent = value - value2;
 
         }
     }
