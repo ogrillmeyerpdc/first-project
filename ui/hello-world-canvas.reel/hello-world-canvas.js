a85c3fef464e50b2ae38c352a50a8e5abc5137f5
ogtopramen <noreply>
2015-01-07 00:01:42 +0000

    Update component ui/hello-world-canvas.reel/hello-world-canvas.js

--- ui/hello-world-canvas.reel/hello-world-canvas.js
+++ ui/hello-world-canvas.reel/hello-world-canvas.js
@@ -18,7 +18,17 @@ exports.HelloWorldCanvas = Component.specialize(/** @lends HelloWorldCanvas# */ 
     draw: {
         value: function() {
             var canvas = this.element.querySelector('canvas');
-            canvas.getContext("2d").fillText("hello", 400, 400);
+            var ctx = canvas.getContext("2d");
+            ctx.font="20px Georgia";
+            
+            var gradient=ctx.createLinearGradient(0,0,canvas.width,0);
+            gradient.addColorStop("0","magenta");
+            gradient.addColorStop("0.5","blue");
+            gradient.addColorStop("1.0","red");
+            // Fill with gradient
+            ctx.fillStyle=gradient;
+            
+            ctx.fillText("hello", 20, 20);
         }
     }
 });
